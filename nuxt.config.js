6477f9abc1961c7352f94cfe8acbe700dd6ab9ae
yinm <yinm.cminor9@gmail.com>
2018-10-27 15:28:29 +0900

    Add settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,20 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    titleTemplate: '%s | Awesome JS SSR Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css'
+      }
+    ]
   },
 
   /*
